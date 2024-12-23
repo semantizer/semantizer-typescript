76c4d882e81105729d86f76547e952436913e577
Maxime Lecoq <maxime@lecoqlibre.fr>
2024-12-23 21:57:52 +0100

    Remove LoaderRdfjs log

--- src/packages/loader-rdfjs/src/LoaderRdfjs.ts
+++ src/packages/loader-rdfjs/src/LoaderRdfjs.ts
@@ -4,7 +4,7 @@ import { DatasetCoreRdfjs, Quad, Loader } from "@semantizer/types";
 export class LoaderRdfjs implements Loader {
 
     public async load(uri: string): Promise<DatasetCoreRdfjs<Quad, Quad>> {
-        console.log("[LoaderRdfjs] loading", uri);
+        // console.log("[LoaderRdfjs] loading", uri);
         const response = await rdfjsFetch<DatasetCoreRdfjs<Quad>, Quad, Quad>(uri);
         return await response.dataset();
     }
